4d627044c964f410ebf061f3d0b4a14bf351fc15
Shahed Ahamed <shahedahamed.official@gmail.com>
2025-11-08 21:44:49 +0600

    Palindrome check functionality

---
+++ practice-45.py
@@ -0,0 +1,10 @@
+a = input("Enter a you wanna check that palidrome word: ")
+
+rev = a[::-1]
+
+if a == rev:
+    print(" The word is a palindrome word")
+else:
+    print("The word is not a palindrome word")
+    
+print("Done babe!")
