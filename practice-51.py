418238d6a26a08cca166c9d74e68ee8e6c49d6ec
Shahed Ahamed <shahedahamed.official@gmail.com>
2025-11-10 21:19:00 +0600

    Update practice-51.py

--- practice-51.py
+++ practice-51.py
@@ -3,4 +3,5 @@ subjects = { "python", "java", "c++", "html", "css" ,"python","java","html","jav
             "c","c++","c#","ruby","php" } #set er moddhe duplicate value thakbe na
 
 print(subjects)
+
 print(len(subjects)) #set er length ber korar jonno len() method use kora hoy
