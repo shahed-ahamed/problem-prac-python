8cea17448166f420b156cde0b938c4d2ba96675e
Shahed Ahamed <shahedahamed.official@gmail.com>
2026-05-31 22:06:02 +0600

    age check for voting eligibility

---
+++ practice-59.py
@@ -0,0 +1,6 @@
+age = int (input ("Enter your age: "))
+while age < 18:
+    print ("You are not old enough to vote.")
+    age = int (input ("Enter your age: "))
+    
+print ("You are old enough to vote.")
